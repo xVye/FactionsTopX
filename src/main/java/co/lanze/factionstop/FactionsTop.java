0d67f8cd307b52c2464d803997f5a266cb46e35f
BuildTools <erikhampus@live.se>
2020-01-29 18:53:49 +0100

    Initial commit

---
+++ src/main/java/co/lanze/factionstop/FactionsTop.java
@@ -0,0 +1,17 @@
+package co.lanze.factionstop;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class FactionsTop extends JavaPlugin {
+
+  @Override
+  public void onEnable() {
+    // Plugin startup logic
+
+  }
+
+  @Override
+  public void onDisable() {
+    // Plugin shutdown logic
+  }
+}
